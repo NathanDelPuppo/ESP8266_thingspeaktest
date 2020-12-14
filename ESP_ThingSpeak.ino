b44f6f4a068abad79b4ade9425142801b04f0d06
Nathan <59972524+NathanDelPuppo@users.noreply.github.com>
2020-12-14 12:40:45 +0100

    Add files via upload

---
+++ ESP_ThingSpeak.ino
@@ -0,0 +1,43 @@
+#include <ThingSpeak.h>
+#include <ESP8266WiFi.h>
+
+const char* ssid     = "Freebox-AAD1EE";
+const char* password = "carente2-fabellis@-exarmata6!-deglutiat#8";
+const int ldrPin = A0; // Defining LDR PIN 
+int input_val = 0;  // Varible to store Input values
+
+
+WiFiClient  client;
+long myChannelNumber = 1256624;
+const char myWriteAPIKey[] = "Y81YHY93ZFAP98PR";
+
+void setup() {
+   Serial.begin(9600); 
+   delay(100);
+   pinMode(ldrPin, INPUT);
+   
+   Serial.println();
+   Serial.println();
+   Serial.print("Connecting to ");
+   Serial.println(ssid);
+  
+   WiFi.begin(ssid, password);
+  
+   while (WiFi.status() != WL_CONNECTED) {
+   delay(200);
+   Serial.print("...");
+   }
+  
+   Serial.println("");
+   Serial.println("WiFi connected");  
+   Serial.println("IP address: ");
+   Serial.println(WiFi.localIP());
+   ThingSpeak.begin(client);
+  }
+
+
+void loop() {  
+   input_val = analogRead(ldrPin);      // Reading Input
+   Serial.print(input_val);
+   ThingSpeak.writeField(myChannelNumber, 1,input_val, myWriteAPIKey);
+}
